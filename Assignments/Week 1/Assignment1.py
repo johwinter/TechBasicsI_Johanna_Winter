1916bb47c000ed7804535b7b190bda4c2d19267c
johwinter <johanna.winter@stud.leuphana.de>
2026-04-16 11:59:40 +0200

    Assignment1.py
    First Assignment: Ascii Art 
    Johanna Winter

--- Assignments/Week 1/Assignment1.py
+++ Assignments/Week 1/Assignment1.py
@@ -1,1 +1,16 @@
+print("""                         
+    /)ii/)                        
+  (o  " )                         
+    |   |                         
+    | o |                         
+    |   |_________||              
+    |      o       |              
+    |  o______o____|              
+    || ||      || ||              
+    || ||      || ||              
+""")                              
 
+
+# My idea was to go for an easy art design that enables me to practice ascii art design without the risk of immediately failing. 
+# I tried to go for an animal design and looked online on Pinterest for inspiration. I have now come up with a variation that is supposed to 
+# resemble a giraffe (but can also be interpreted as an alpaka...). 
